89282b7588d6fa91d82fa47fb225d647e64fe4ed
Ganesh <ganesh.csa.iisc@gmail.com>
2019-07-30 23:04:26 -0700

    Create preferencesActivity.java

---
+++ app/src/main/java/com/example/wallpaper_weather/preferencesActivity.java
@@ -0,0 +1,14 @@
+package com.example.wallpaper_weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class preferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+    }
+}
