9ed50b96402cedc8a6fef5a8a249ff3df5c72524
Adham Abozaeid <adham.abozaeid@gmail.com>
2019-08-11 19:06:24 -0700

    Going back to original page from preference page
    Signed-off-by: Adham Abozaeid <adham.abozaeid@gmail.com>

--- app/src/main/java/com/example/wallpaper_weather/preferencesActivity.java
+++ app/src/main/java/com/example/wallpaper_weather/preferencesActivity.java
@@ -16,5 +16,6 @@ public class preferencesActivity extends AppCompatActivity {
 
     public void applySettings(View view) {
         //startActivity(new Intent(this, MainActivity.class) );
+        finish();
     }
 }
